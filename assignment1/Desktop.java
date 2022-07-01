747dd41b264a4b0ea631389d875ad2d9170dc1fd
lalitha-rvs <107381939+lalitha-rvs@users.noreply.github.com>
2022-07-02 00:38:02 +0530

    Update Desktop.java

--- assignment1/Desktop.java
+++ assignment1/Desktop.java
@@ -1,14 +1,14 @@
 package org.system.assignment1;
 
-public class Desktop extends Computer{
+public class Desktop extends Computer{    // class Desktop  inherits class Computer 
 	public void deskTopSize() {
 		int size=14;
 	
 		System.out.println("desktop size is "+size);}
 	
 public static void main(String[] args) {
-	Desktop obj=new Desktop();
-	obj.ComputerModel();
-	obj.deskTopSize();
+	Desktop obj=new Desktop();//creating an object of class Desktop 
+	obj.ComputerModel();       //call to method in computer class
+	obj.deskTopSize();         //call to method in Desktop class
 }
 }
